aad01ae6dd1e965a7018c3a80a70c51d8cc4b2d6
Kanchana.Tejaswy <mail2tejaswy@gmail.com>
2025-08-03 17:11:41 +0530

    added navigation bar with menu

--- src/Component/navbar.js
+++ src/Component/navbar.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ' ./navbar.css';
+import MenuIcon from 'https://mui.com/material-ui/react-menu/#LongMenu.js'
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className="navbar-left">
+                <div className=" navbarHamberger">
+                    <MenuIcon sx={{colour:"white"}} />
+                </div>
+            </div>
+        </div>
     )
 }
