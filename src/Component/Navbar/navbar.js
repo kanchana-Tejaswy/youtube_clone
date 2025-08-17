ecd3e04a091b8c091b9e2e29cde43a447996b95a
Kanchana.Tejaswy <mail2tejaswy@gmail.com>
2025-08-17 11:09:58 +0530

    added youtube logo with name in nav bar

--- src/Component/Navbar/navbar.js
+++ src/Component/Navbar/navbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './navbar.css';
 import MenuIcon from '@mui/icons-material/Menu';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 const Navbar = () => {
     return (
         <div className='navbar'>
             <div className="navbar-left">
                 <div className="navbarHamberger">
                     <MenuIcon sx={{color:"white"}}/>
+                </div>
+                <div className="navbar_youtubeImg">
+                    <YouTubeIcon sx={{ fontSize: "34px"}} className='navbar_youtubeImage' />
+                    <div className='navbar_utubeTitle'>YouTube</div>
                 </div>
             </div>
         </div>
