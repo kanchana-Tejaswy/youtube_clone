f8f6a6d3450d3d9634487b897bee7b44c6e5c354
Kanchana.Tejaswy <mail2tejaswy@gmail.com>
2025-08-17 15:36:32 +0530

    added search bar with icon

--- src/Component/Navbar/navbar.js
+++ src/Component/Navbar/navbar.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './navbar.css';
 import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+
 const Navbar = () => {
     return (
         <div className='navbar'>
+
             <div className="navbar-left">
                 <div className="navbarHamberger">
                     <MenuIcon sx={{color:"white"}}/>
@@ -14,6 +18,14 @@ const Navbar = () => {
                     <div className='navbar_utubeTitle'>YouTube</div>
                 </div>
             </div>
+            <div className="navbar-middel">
+                <div className="navbar_searchBox">
+                    <input type= 'text' placeholder='Search' className='navbar_searchBoxInput' />
+                     <div className="navbar_searchIconBox"><SearchIcon sx={{ fontsize: "28px", color:"white" }} /></div>
+                </div>
+                
+            </div>
+
         </div>
     )
 }
